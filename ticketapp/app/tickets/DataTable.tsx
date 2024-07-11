197ce34be46c1ff5b68ab44f3e11d9e817065d48
kaiotcp1 <kaiotcp1@gmail.com>
2024-07-11 16:57:14 -0300

    feat(ui): table base structure and date format

---
+++ ticketapp/app/tickets/DataTable.tsx
@@ -0,0 +1,54 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Ticket } from '@prisma/client'
+import React from 'react'
+
+interface Props {
+    tickets: Ticket[]
+};
+
+const DataTable = ({ tickets }: Props) => {
+    return (
+        <div className='w-full mt-5'>
+            <div className='rounded-md sm:border'>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>
+                                Title
+                            </TableHead>
+                            <TableHead>
+                                Status
+                            </TableHead>
+                            <TableHead>
+                                Priority
+                            </TableHead>
+                            <TableHead>
+                                Created At
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {tickets ? tickets.map((ticket) => (
+                            <TableRow key={ticket.id} data-href='/'>
+                                <TableCell>{ticket.title}</TableCell>
+                                <TableCell>{ticket.status}</TableCell>
+                                <TableCell>{ticket.priority}</TableCell>
+                                <TableCell>{ticket.createdAt.toLocaleDateString('pt-br', {
+                                    year: '2-digit',
+                                    month: '2-digit',
+                                    day: '2-digit',
+                                    hour: 'numeric',
+                                    minute: '2-digit',
+                                    hourCycle: 'h23',
+                                })}</TableCell>
+
+                            </TableRow>
+                        )) : null}
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
+    )
+}
+
+export default DataTable
--- ticketapp/app/tickets/page.tsx
+++ ticketapp/app/tickets/page.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import prisma from '@/prisma/db';
+import DataTable from "./DataTable";
 
 const Tickets = async () => {
   const tickets = await prisma.ticket.findMany();
 
-  console.log(tickets);
-
   return <div>
-
-    {tickets.map((ticket) => (
-      <div key={ticket.id}>{ticket.title}</div>
-    ))}
+    <DataTable tickets={tickets} />
   </div>;
 };
 
