8c455c43ee107250668bc6458f8d451b38432ff5
kaiotcp1 <kaiotcp1@gmail.com>
2024-08-22 18:19:24 -0300

    feat(api): add logic to update assigned user for tickets

--- ticketapp/app/api/tickets/[id]/route.ts
+++ ticketapp/app/api/tickets/[id]/route.ts
@@ -1,4 +1,4 @@
-import { ticketSchema } from "@/ValidationSchemas/tickets";
+import { ticketPatchSchema } from "@/ValidationSchemas/tickets";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/db";
 
@@ -10,7 +10,7 @@ interface Props {
 
 export async function PATCH(request: NextRequest, { params }: Props) {
     const body = await request.json();
-    const validation = ticketSchema.safeParse(body);
+    const validation = ticketPatchSchema.safeParse(body);
 
     if (!validation.success) {
         return NextResponse.json(validation.error.format(), { status: 400 });
@@ -23,6 +23,10 @@ export async function PATCH(request: NextRequest, { params }: Props) {
     if (!ticket) {
         return NextResponse.json({ error: "Ticket Not Found" }, { status: 404 });
     };
+
+    if (body?.assignedToUserId) {
+        body.assignedToUserId = parseInt(body.assignedToUserId);
+    }
 
     const updateTicket = await prisma.ticket.update({
         where: { id: ticket.id },
