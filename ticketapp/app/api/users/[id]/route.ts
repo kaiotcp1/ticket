b4fee395c9ac2adf3f4ab4ffc95d5d28e0e69259
kaiotcp1 <kaiotcp1@gmail.com>
2024-08-23 16:53:39 -0300

    feature(api): add protection to api routes

--- ticketapp/app/api/users/[id]/route.ts
+++ ticketapp/app/api/users/[id]/route.ts
@@ -3,18 +3,26 @@ import { userSchema } from '@/ValidationSchemas/users';
 import { User } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from "next-auth";
+import options from '../../auth/[...nextauth]/options'
+
 
 interface Props {
     params: { id: string }
 }
 
 export async function PATCH(request: NextRequest, { params }: Props) {
+
+    const session = await getServerSession(options);
+
+    if (!session) return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+
+    if (session.user.role !== 'ADMIN') return NextResponse.json({ error: 'Only Admins Can Update Users.' }, { status: 401 });
+
     const body: Partial<User> = await request.json();
     const validation = userSchema.safeParse(body);
 
-    if (!validation.success) {
-        return NextResponse.json(validation.error.format(), { status: 400 });
-    };
+    if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 });
 
     const user = await prisma.user.findUnique({
         where: {
@@ -22,14 +30,14 @@ export async function PATCH(request: NextRequest, { params }: Props) {
         },
     });
 
-    if (!user) {
-        return NextResponse.json({ error: 'User Not Found.' }, { status: 404 });
-    };
+    if (!user) return NextResponse.json({ error: 'User Not Found.' }, { status: 404 });
 
     if (body?.password && body.password != '') {
         const hashPassword = await bcrypt.hash(body.password, 10);
         body.password = hashPassword;
+        console.log('update' + body.password);
     } else {
+        console.log('delete' + body.password);
         delete body.password;
     }
 
@@ -39,9 +47,7 @@ export async function PATCH(request: NextRequest, { params }: Props) {
                 username: body.username
             },
         });
-        if (duplicateUsername) {
-            return NextResponse.json({ message: 'Duplicate Username' }, { status: 409 });
-        };
+        if (duplicateUsername) return NextResponse.json({ message: 'Duplicate Username' }, { status: 409 });
     }
 
     const updateUser = await prisma.user.update({
@@ -53,5 +59,7 @@ export async function PATCH(request: NextRequest, { params }: Props) {
         },
     });
 
-    return NextResponse.json(updateUser, { status: 200 });
+    const { password, ...userWithoutPassword } = updateUser;
+
+    return NextResponse.json(userWithoutPassword, { status: 200 });
 };
